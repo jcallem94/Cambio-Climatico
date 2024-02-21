edacbb98ab24c09e63f8462469f624aafa372e97
jcallem94 <jcallem94@gmail.com>
2024-02-21 13:30:02 -0500

    Corrección código

--- JavaS.js
+++ JavaS.js
@@ -1,7 +1,6 @@
 const menuHamburguesa = document.querySelector('.menu-hamburguesa');
 const barraLateral = document.querySelector('.barra-lateral');
-
 menuHamburguesa.addEventListener('click', () => {
-  menuHamburguesa.classList.toggle('.activo');
-  barraLateral.classList.toggle('.activo');
+  menuHamburguesa.classList.toggle('active');
+  barraLateral.classList.toggle('active'); // Sincroniza la clase con el botón
 });
